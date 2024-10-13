45063383c36bdab08cc03ac5138d12b0c50f7566
Jacob Shing <zhanhoshing@icloud.com>
2024-10-13 10:40:09 +0800

    feat: game ui improvement
    Signed-off-by: Jacob Shing <zhanhoshing@icloud.com>

--- speed_slide/__init__.py
+++ speed_slide/__init__.py
@@ -2,16 +2,29 @@ from tui import Screen
 import tui.transitions as transitions
 import speed_slide.__game_consts as consts
 from speed_slide.game_scenes import *
+import random
+
+from tui.transitions import scatter
+
+
+def __random_transition():
+    return random.choice((
+        transitions.scatter(200),
+        transitions.wipe_up_to_down,
+        transitions.wipe_down_to_up,
+        transitions.slide_from_top,
+        transitions.slide_from_bottom,
+    ))
 
 
 def game():
     game_screen = Screen(consts.__SCREEN_WIDTH, consts.__SCREEN_HEIGHT)
 
-    game_screen.transition_into_scene(InitScene(consts.__SCREEN_WIDTH, consts.__SCREEN_HEIGHT), transitions.direct)
+    game_screen.transition_into_scene(InitScene(consts.__SCREEN_WIDTH, consts.__SCREEN_HEIGHT))
     game_screen.play_scene()
 
     game_screen.transition_into_scene(TitleScene(consts.__SCREEN_WIDTH, consts.__SCREEN_HEIGHT), transitions.scatter(200), 0.02)
     game_screen.play_scene()
 
-    game_screen.transition_into_scene(MainGameMenu(consts.__SCREEN_WIDTH, consts.__SCREEN_HEIGHT), transitions.slide_from_bottom, 0.02)
+    game_screen.transition_into_scene(MainGameMenu(consts.__SCREEN_WIDTH, consts.__SCREEN_HEIGHT), __random_transition(), 0.02)
     game_screen.play_scene()
--- speed_slide/game_scenes/main_game_menu_scene.py
+++ speed_slide/game_scenes/main_game_menu_scene.py
@@ -15,7 +15,7 @@ class MainGameMenu(Scene):
 
 
     def play(self):
-        menu_dw = DialogueWindow(60, 20, 25, 5, title="Speed Slide Game Menu")
+        menu_dw = DialogueWindow(60, 20, 25, 5, title="SpeedSlide Game Menu")
         menu_dw.controls.extend(
             [
                 # header
@@ -23,7 +23,31 @@ class MainGameMenu(Scene):
                 # menu options
                 TxtLabel(50, 5, 5, 5, text='- [N]ew Game\n- [H]elp\n- [Q]uit', auto_size=True,
                          padding_top=1, padding_bottom=1, padding_left=1, padding_right=1, draw_borders=True),
+                TxtLabel(50, 1, 5, 11, text='Only the first character of your input will be read.',
+                         auto_size=True)
             ]
         )
+        def get_user_input(scene: Scene):
+            valid_inputs = ['N', 'H', 'Q']
+            while True:
+                user_option = input('Select an option: ').upper()[:1]
+                if user_option.upper() in valid_inputs:
+                    break
 
-        user_option = self.show_dialogue(menu_dw, lambda: input('Option: '))
+                # show error message
+                invalid_option_dw = DialogueWindow(30, 10, 40, 8, title='ERROR!')
+                invalid_option_dw.controls.append(
+                    TxtLabel(28, 5, 1, 4, text=f'Option \"{user_option}\" is invalid! Please try again.',
+                         padding_left = 2, padding_right = 2, auto_size=True)
+                )
+                scene.show_dialogue(invalid_option_dw, lambda _: input('Press any key to continue...'))
+
+            return user_option
+
+        operations = {
+            'N': lambda: print('New Game'),
+            'H': lambda: print('Help'),
+            'Q': lambda: print('Quit')
+        }
+        menu_dw_output = self.show_dialogue(menu_dw, get_user_input)
+        operations[menu_dw_output.upper()]()
