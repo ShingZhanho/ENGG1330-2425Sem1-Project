85096bdca28066c113fe0406d937835c8c923167
Jacob Shing <zhanhoshing@icloud.com>
2024-10-19 11:27:39 +0800

    refac: remove InitScene
    Signed-off-by: Jacob Shing <zhanhoshing@icloud.com>

--- speed_slide/game_scenes/__init__.py
+++ speed_slide/game_scenes/__init__.py
@@ -1,3 +1,2 @@
-from speed_slide.game_scenes.init_scene import InitScene
 from speed_slide.game_scenes.title_scene import TitleScene
 from speed_slide.game_scenes.main_game_menu_scene import MainGameMenu
