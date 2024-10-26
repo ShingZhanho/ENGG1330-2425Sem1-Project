406ca6578b7398a00ab24e0de7e5addbe74c968f
Jacob Shing <zhanhoshing@icloud.com>
2024-10-26 14:00:06 +0800

    feat: help page
    Signed-off-by: Jacob Shing <zhanhoshing@icloud.com>

--- speed_slide/game_scenes/help_scene.py
+++ speed_slide/game_scenes/help_scene.py
@@ -1,7 +1,7 @@
-from tui import Scene, ForegroundColours, RichFormatText
+from tui import Scene, ForegroundColours, RichFormatText, TextFormats
 from tui.controls import DialogueWindow, TxtLabel
 from speed_slide.__game_consts import _Constants as Constants
-from speed_slide.io import safe_input
+from speed_slide.io import safe_input, beep
 
 
 class HelpScene(Scene):
@@ -18,17 +18,29 @@ class HelpScene(Scene):
         """
         Displays the help information page-by-page.
         """
-        page_counter: int = 0
-        total_pages: int = 1
-        while page_counter < total_pages:
-            page_counter += 1
+        page_counter: int = 1
+        total_pages: int = 2 # UPDATE THIS VALUE WHEN ADDING MORE PAGES
+        while page_counter <= total_pages:
             self.show_dialogue(HelpScene.__get_page(page_counter), None)
             option = safe_input(
-                RichFormatText(f'(Page {page_counter}/{total_pages}) Press enter for next page, or q to return to menu.')
-                 .set_format(0, slice(None), ForegroundColours.BLUE)
-            )
+                RichFormatText(f'(Page {page_counter}/{total_pages}) Commands: [N] - Next page; [P] - '
+                               f'Previous page; [Q] - Returns to main menu\n')
+                 .set_format(0, slice(None), ForegroundColours.MAGENTA)
+            ).lower()
             if option == 'q':
                 return
+            elif option == 'p':
+                if page_counter == -1:
+                    beep()
+                else:
+                    page_counter -= 1
+            elif option == 'n':
+                if page_counter == total_pages:
+                    beep()
+                else:
+                    page_counter += 1
+            else:
+                beep()
 
     @classmethod
     def __get_page(cls, page_num: int) -> DialogueWindow:
@@ -36,16 +48,29 @@ class HelpScene(Scene):
         Construct the first page DialogueWindow and returns it.
         :param page_num: The page number.
         """
-        dw = None
         dw_width = Constants.SCREEN_WIDTH - 6
         dw_height = Constants.SCREEN_HEIGHT - 4
         dw_x = 3
         dw_y = 2
+        dw = DialogueWindow('dw_name_pending', dw_width, dw_height, dw_x, dw_y, 0, '',
+                            border_colour=ForegroundColours.CYAN)
+        accumulated_y = 2 # calculating the y-coordinate of the controls
         match page_num:
+            # ================================= START [PAGE -1] ==============================
+            case -1:
+                dw.controls_id='dw_hidden_page'
+
+                lbl_para1 = TxtLabel('lbl_para1', dw_width - 4, 1, 2, accumulated_y, 0, auto_size=True,
+                                        text="YOU GOTTA BE JOKING! Why would you want to go page -1?!\n\n\n"
+                                             "Anyway, here's a hidden trick for you:\n\n"
+                                             "Type \'/give-up?\' at the start of a level will give you the solution. "
+                                             "REMEMBER! The solution only works when the board has not been touched.")
+
+                dw.controls.append(lbl_para1)
+            # ================================= START [PAGE 1] ===============================
             case 1:
-                dw = DialogueWindow('dw_p1', dw_width, dw_height, dw_x, dw_y,0,'',
-                                    border_colour=ForegroundColours.CYAN)
-                accumulated_y = 2
+                dw.control_id = 'dw_page1'
+
                 lbl_para1 = TxtLabel('lbl_para1', dw_width - 4, 1, 2, accumulated_y, 0, auto_size=True,
                                      text="The game starts with a game board of difficulty 1, which is a 3x3 grid. "
                                           "The grid is filled with numbers from 1 to 8, and one empty block at the "
@@ -69,9 +94,46 @@ class HelpScene(Scene):
 
                 lbl_para3 = TxtLabel('lbl_para3', dw_width - 4, 1, 2, accumulated_y, 0, auto_size=True,
                                      text="Your main goal is to slide the blocks around to restore the numbers in their"
-                                          " original order. During every move, you can only slide the block that is "
+                                          " original order. In each move, you can only slide the block that is "
                                           "directly adjacent to the empty space, i.e. on top, bottom, left, or right of "
-                                          "the empty space.")
+                                          "the empty space.\n\n"
+                                          "To make a move, enter the number of the block you want to slide into the "
+                                          "the empty space, then press enter. "
+                                          "If you want to leave the game immediately, type the command \'/quit\' and "
+                                          "press enter. If you simply want to give up and see your final score, you "
+                                          "can use the command \'/surrender\'.")
 
                 dw.controls.extend([lbl_para1, lbl_para2, lbl_para3])
+            # ================================= END [PAGE 1] =================================
+
+            # ================================= START [PAGE 2] ===============================
+            case 2:
+                dw.control_id = 'dw_page2'
+
+                lbl_para1 = TxtLabel('lbl_para1', dw_width - 4, 1, 2, accumulated_y, 0, auto_size=True,
+                                     text="On the right hand side of the screen, you will see three numbers:\n\n"
+                                          "  - Target moves: Your target of moves to solve the puzzle.\n"
+                                          "  - Max. moves: The maximum number of moves allowed in the level.\n"
+                                          "  - Moves: The number of moves you have made so far.")
+                accumulated_y += lbl_para1.height + 2
+
+                lbl_para2 = TxtLabel('lbl_para2', dw_width - 4, 1, 2, accumulated_y, 0, auto_size=True,
+                                     text="Try to make as few moves as possible to solve the puzzle!\n\n"
+                                          "If you make moves less than or equal to the target moves, you will earn extra"
+                                          " points and get promoted to the next difficulty level.\n\n"
+                                          "If you solve the the puzzle within the maximum moves but exceed the target"
+                                          " moves, you will still get basic points for solving the puzzle, but you will"
+                                          " stay on the same difficulty level.\n\n"
+                                          "If you cannot solve the puzzle within the maximum moves, the game ends "
+                                          "immediately.")
+                accumulated_y += lbl_para2.height + 2
+
+                lbl_para3 = TxtLabel('lbl_para3', dw_width - 4, 1, 2, accumulated_y, 0, auto_size=True,
+                                     text="WATCH OUT! If you make moves more than the target moves, there will be some"
+                                          " random events happening as the game goes on. They can either give you bonus"
+                                          " scores or take away your points. A wicked witch is waiting to haunt you!")
+                lbl_para3.formatted_text.set_format(0, slice(11), ForegroundColours.RED, text_format=TextFormats.BOLD)
+
+                dw.controls.extend([lbl_para1, lbl_para2, lbl_para3])
+            # ================================= END [PAGE 2] =================================
         return dw
